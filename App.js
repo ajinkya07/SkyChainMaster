ab55bc7773b42c3067a5e62b0e08d9cf66a72b69
ajinkya07 <35160476+ajinkya07@users.noreply.github.com>
2020-09-26 20:42:15 +0530

    firebase notification

--- App.js
+++ App.js
@@ -6,11 +6,10 @@ import {View, Platform} from 'react-native';
 import {Provider} from 'react-redux';
 import configureStore from '@redux/store';
 import SplashScreen from 'react-native-splash-screen'
-
- import firebase from 'react-native-firebase';
- import { Notification, NotificationOpen } from 'react-native-firebase';
  import AsyncStorage from '@react-native-community/async-storage';
-
+ import { fcmService } from './DS/FCMService';
+ import { local } from './DS/Local';
+ 
 
 const store = configureStore();
 
@@ -24,147 +23,40 @@ class App extends React.Component {
     //  console.disableYellowBox = true;
      SplashScreen.hide()
 
-    await this.checkPermission();
-
-    this.createNotificationListeners()
-    //  firebase.notifications().setBadge(0)
-    firebase.notifications().removeAllDeliveredNotifications()
+     fcmService.registerAppWithFCM();
+     fcmService.register(this.onRegister, this.onNotification, this.onOpenNotification);
+     local.configure(this.onOpenNotification)
+ 
 
   };
-
-  checkPermission = async () => {
-    const enabled = await firebase.messaging().hasPermission();
-    if (enabled) {
-      this.getFcmToken();
-    } else {
-      this.requestPermission();
-    }
-  }
-
-
-  getFcmToken = async () => {
-    const fcmToken = await firebase.messaging().getToken();
-    if (fcmToken) {
-      console.warn("fcmToken", fcmToken);
-
-      AsyncStorage.setItem('fcmToken', fcmToken)
-
-    } else {
-    }
-  }
-
-
-  requestPermission = async () => {
-    try {
-      await firebase.messaging().requestPermission();
-      // User has authorised
-    } catch (error) {
-      // User has rejected permissions
-    }
-  }
-
-
-  createNotificationListeners = async () => {
-    this.notificationDisplayedListener = firebase.notifications().onNotificationDisplayed((notification: Notification) => {
-      console.warn('NOTIFICATION_DISPALYED_LISTENER', notification)
-    });
-
-    this.notificationListener = firebase.notifications().onNotification((notification: Notification) => {
-      console.warn('NOTIFICATION_ON_NOTIFCATION', notification)
-      if (Platform.OS === "android") {
-        console.warn("for android");
-        this.androidMessageHandle(notification);
-        
-      
-      }
-       else if(Platform.OS ==='ios'){
-         console.warn("ios iosMessageHandle")
-        this.iosMessageHandle(notification);
-      }
   
-      // store.dispatch({ 
-      //   type: "PICKUPS_FETCH_PICKUP_DATES",
-      // });
-    });
+  onRegister = (token) => {
+    console.warn("[App] Token", token);
 
-    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen: NotificationOpen) => {
-      const { data, notificationId } = notificationOpen.notification;
-      //const actionType = getNextScreen(data)
-      //this.redirectToNextScreenAndMarkAsRead(data.gsNotificationId, actionType, data.payloadID )
-    });
+    AsyncStorage.setItem('fcmToken',token)
+  }
 
-    const notificationOpen = await firebase.notifications().getInitialNotification();
-    if (notificationOpen) {
 
-      const action = notificationOpen.action;
-
-      // Get information about the notification that was opened
-      const notification: Notification = notificationOpen.notification;
-
-    
-
-      const { data, notificationId } = notification;
-      //const actionType = getNextScreen(data)
-      //this.redirectToNextScreenAndMarkAsRead(data.gsNotificationId, actionType , data.payloadID )
+   onNotification = (notify) => {
+     console.warn("[App] onNotification", notify);
+    const options = {
+      soundName: 'default',
+      playSound: true,
     }
+
+    local.showNotification(
+      0,
+      notify.title,
+      notify.subtitle,
+      notify,
+      options,
+    )
   }
 
-
-
-
-  //for android notification  sound icon 
-  androidMessageHandle = notification => {
-    const { title, subtitle, data} = notification.data;
-
-    // Build a channel(android only)
-    const channel = new firebase.notifications.Android.Channel('test-channel', 'test-channel', firebase.notifications.Android.Importance.Max)
-    .setDescription('My apps test channel');
-
-    console.warn("channel",channel)
-
-    firebase.notifications().android.createChannel(channel);
-    // .setNotificationId(data.gsNotificationId)
-
-    console.warn("notification",notification)
-    console.warn("notification.data",notification.data)
-
-
-    notification  
-      .setTitle(title)
-      .setBody(subtitle)
-      .setSound('default')
-      .android.setChannelId("test-channel")
-      .android.setAutoCancel(true)
-      .android.setPriority(firebase.notifications.Android.Priority.Max)
-      //.android.setClickAction(notification.android.clickAction)
-     // .android.setSmallIcon("ic_notification") // create this icon in Android Studio
-      //.android.setColor("#0d185c")
-    firebase.notifications().displayNotification(notification);
-    //firebase.notifications().setBadge(parseInt(data.badge))
+   onOpenNotification = async (notify) => {
   
-  };
-
-
-  iosMessageHandle = notification => {
-    console.warn("iosMessageHandle",notification)
-
-    const { title, subtitle, data} = notification;
-    notification
-    .setTitle(title)
-    .setBody(subtitle)
-    .setSound('default')
-    //.setData({actionType: data.actionType,})
-    // .ios.setBadge(parseInt(data.badge));
-    firebase.notifications().displayNotification(notification);
-  };
-
-
-  componentWillUnmount() {
-    this.notificationDisplayedListener();
-    this.notificationListener();
-    this.notificationOpenedListener();
+    console.warn('notify', notify);
   }
-
 
 
 
@@ -182,15 +74,4 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-
-
-// Android : 
-
-// {"to":"ec4mFrzHReqd-rjxUK1npx:APA91bFV8IgUGhcXdPhZeR-hZhlMtcacVvpeI5GkwHhNdLB-bNVTSjiOrTgl0YlKW
-// qjdAeFcwL1X_sdMxvPZ-ZCHLvfK1Zvnbkd2CWxixHg9JT8YfgqewRQIwuKs3fCKZM5SvgU5HecC",
-// "priority":"high",
-// "click_action":"order_details",
-// "data":{"title":"Sky Chains","subtitle":"daa","order_id":"0"}}
 
